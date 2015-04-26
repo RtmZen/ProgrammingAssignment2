c342c934736a98afa3035ebbc29a4968c2c10e58
RtmZen <artem.ed@gmx.com>
2015-04-27 02:14:31 +0300

    modified: comments in cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,12 +1,14 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Following code presents a complex of two functions that use
+# scoping rules to cache a result of potentially resource-
+# intensive computation
 
-## Write a short comment describing this function
+# makeCacheMatrix - a function that creates a special "matrix"
+# object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-   m <- NULL
+   m <- NULL  # Erase an old result
    set <- function(y) {
-      x <<- y
+      x <<- y  # Store the last result in a cashe
       m <<- NULL
    }
    get <- function() x
@@ -18,7 +20,10 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+# cacheSolve - a function that computes the inverse of the "matrix"
+# object returned by makeCacheMatrix. If the inverse has already
+# been calculated (and the matrix has not changed), then
+# the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
    m <- x$getinverseMatrix()
@@ -27,7 +32,8 @@ cacheSolve <- function(x, ...) {
       return(m)
    }
    data <- x$get()
-   m <- (data, ...)^-1
+   m <- (data, ...)^-1  # Calculate inverse of matrix
+                        # returned stored in 'data'
    x$setinverseMatrix(m)
-   m  ## Return a matrix that is the inverse of 'x'
+   m  # Return a matrix that is the inverse of 'x'
 }
